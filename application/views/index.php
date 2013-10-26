30a890636059f5cab08a7446ab3d5ed01c18e05d
bergren2 <basstheorychaos@gmail.com>
2013-10-26 11:44:57 -0500

    add title and some basic fonts

--- application/views/index.php
+++ application/views/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Welcome to Fork It!</title>
+    <title>Working Title... | Chicago Game Jam 2013</title>
     
     <!-- Resources -->
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
@@ -16,10 +16,12 @@
     <script type="text/javascript" src="js/scenes/loading.js"></script>
     <script type="text/javascript" src="js/game.js"></script>
     <link href="css/common.css" rel="stylesheet" type="text/css">
-    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Scada' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Eater' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Annie+Use+Your+Telescope' rel='stylesheet' type='text/css'>
 </head>
 <body>
+  <h1>Working Title...</h1>
+  <h2>Chicago Game Jam 2013</h2>
     <div id="container">
         <div id="cr-stage"></div>
     </div>
