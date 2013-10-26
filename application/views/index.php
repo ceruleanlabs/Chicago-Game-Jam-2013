9950b6a9363ddab98fd4a8dd1eda87526713ee48
bergren2 <basstheorychaos@gmail.com>
2013-10-26 12:08:01 -0500

    add game info box

--- application/views/index.php
+++ application/views/index.php
@@ -1,29 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <title>Working Title... | Chicago Game Jam 2013</title>
-    
-    <!-- Resources -->
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
-    <script type="text/javascript" src="js/utilities/underscore-min.js"></script>
-    <script type="text/javascript" src="js/crafty.js"></script>
-    <script type="text/javascript" src="js/components/player.js"></script>
-    <script type="text/javascript" src="js/components/movement.js"></script>
-    <script type="text/javascript" src="js/components/graphics.js"></script>
-    <script type="text/javascript" src="js/components/game_objects.js"></script>
-    <script type="text/javascript" src="js/levelManager.js"></script>
-    <script type="text/javascript" src="js/scenes/loading.js"></script>
-    <script type="text/javascript" src="js/game.js"></script>
-    <link href="css/common.css" rel="stylesheet" type="text/css">
-    <link href='http://fonts.googleapis.com/css?family=Eater' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Annie+Use+Your+Telescope' rel='stylesheet' type='text/css'>
+  <meta charset="utf-8">
+  <title>Working Title... | Chicago Game Jam 2013</title>
+
+  <!-- Resources -->
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+  <script type="text/javascript" src="js/utilities/underscore-min.js"></script>
+  <script type="text/javascript" src="js/crafty.js"></script>
+  <script type="text/javascript" src="js/components/player.js"></script>
+  <script type="text/javascript" src="js/components/movement.js"></script>
+  <script type="text/javascript" src="js/components/graphics.js"></script>
+  <script type="text/javascript" src="js/components/game_objects.js"></script>
+  <script type="text/javascript" src="js/levelManager.js"></script>
+  <script type="text/javascript" src="js/scenes/loading.js"></script>
+  <script type="text/javascript" src="js/game.js"></script>
+  <link href="css/common.css" rel="stylesheet" type="text/css">
+  <link href='http://fonts.googleapis.com/css?family=Eater' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Annie+Use+Your+Telescope' rel='stylesheet' type='text/css'>
 </head>
 <body>
   <h1>Working Title...</h1>
   <h2>Chicago Game Jam 2013</h2>
     <div id="container">
-        <div id="cr-stage"></div>
+      <div id="cr-stage"></div>
+      <div id="game-info">It's a metaphor for death.</div>
     </div>
 </body>
 </html>
