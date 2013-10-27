5540e5b69b6bbe8c8f60a573171db9bec489b43b
bergren2 <basstheorychaos@gmail.com>
2013-10-27 02:14:28 -0500

    load map 1 first instead of our test map

--- js/scenes/loading.js
+++ js/scenes/loading.js
@@ -86,7 +86,7 @@ Crafty.scene("loading", function () {
     Crafty.modules({ SpriteColor: 'RELEASE' }, function () {
         // Simulate a load time for now to make sure this works
         setTimeout(function () {
-          levelManager.loadMap(0, null); //when everything is loaded, run the main scene
+          levelManager.loadMap(1, null); //when everything is loaded, run the main scene
         }, 500);
     });
   });
