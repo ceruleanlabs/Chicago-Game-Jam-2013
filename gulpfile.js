a163b2972728d951b8365efca23413ff3491352b
bergren2 <dan@heymrbass.com>
2016-07-02 21:31:52 -0500

    init Gulp build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require("gulp");
 var ghPages = require("gulp-gh-pages");
 
-gulp.task("deploy", function() {
+gulp.task("deploy", function () {
   return gulp.src("./dist/**/*").pipe(ghPages());
+});
+
+gulp.task("build", function () {
+  return gulp.src("./src/index.html").pipe(gulp.dest("./dist/"));
 });
